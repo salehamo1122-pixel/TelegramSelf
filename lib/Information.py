50641021818952f19cce376d366946dd6efeaa03
salehamo1122-pixel <salehamo1122@gmail.com>
2026-07-18 03:57:41 +0330

    Update API credentials and admin user ID

--- lib/Information.py
+++ lib/Information.py
@@ -1,10 +1,10 @@
 from .library import *
 
-admin_user_id = 123456789 #<--- آیدی ادمین
-api_id = 123456789 #<--- آی پی آی آیدی
-api_hash = 'abcdefghigklmnopqrstuvwxyz' #<--- ای پی آی هش
-helper_username = 'BotUserName' #<--- یوزر ربات هلپر بدون @
-bot_token = 'abcdefghigklmnopqrstuvwxyz:466546' #<--- توکن ربات هلپر
+admin_user_id = 6642473599 #<--- آیدی ادمین
+api_id = 32652890 #<--- آی پی آی آیدی
+api_hash = 'd643d86f2a2f379addc4c4aa46e81d6c' #<--- ای پی آی هش
+helper_username = 'Salselfbot' #<--- یوزر ربات هلپر بدون @
+bot_token = '8814285772:AAHc4E_3ixlNeSI7jftK6rJO5aYgGao057U' #<--- توکن ربات هلپر
 
 client_id = '01e7dc6b41c3471b94efe87abeb05919'
 client_secret = '4f5f93af1ced4b0d9ba8440606803639'
